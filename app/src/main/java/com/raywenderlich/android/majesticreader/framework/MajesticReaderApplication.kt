cf46cecc598c182236e8d7ffff2d614fe3c62045
Pablo F. Guerra <pabloimrik17@gmail.com>
2021-05-08 23:08:26 +0200

    Added manual DI

--- app/src/main/java/com/raywenderlich/android/majesticreader/framework/MajesticReaderApplication.kt
+++ app/src/main/java/com/raywenderlich/android/majesticreader/framework/MajesticReaderApplication.kt
@@ -31,15 +31,36 @@
 package com.raywenderlich.android.majesticreader.framework
 
 import android.app.Application
+import com.raywenderlich.android.majesticreader.data.BookmarkRepository
+import com.raywenderlich.android.majesticreader.data.DocumentRepository
+import com.raywenderlich.android.majesticreader.interactors.*
 
 class MajesticReaderApplication : Application() {
 
   override fun onCreate() {
     super.onCreate()
 
+      val bookmarkRepository = BookmarkRepository(
+          RoomBookmarkDataSource(this)
+      )
+
+      val documentRepository = DocumentRepository(
+          RoomDocumentDataSource(this),
+          InMemoryOpenDocumentDataSource()
+      )
+
     MajesticViewModelFactory.inject(
         this,
-        Interactors()
+        Interactors(
+            AddBookmark(bookmarkRepository),
+            GetBookmarks(bookmarkRepository),
+            RemoveBookmark(bookmarkRepository),
+            AddDocument(documentRepository),
+            GetDocuments(documentRepository),
+            RemoveDocument(documentRepository),
+            GetOpenDocument(documentRepository),
+            SetOpenDocument(documentRepository)
+        )
     )
   }
 }
