ff880cae3a5b7a5522fa4c39b02c2e947467f552
Pablo F. Guerra <pabloimrik17@gmail.com>
2021-05-08 23:32:54 +0200

    Fixed SetOpenDocument use case

--- core/src/main/java/com/raywenderlich/android/majesticreader/interactors/SetOpenDocument.kt
+++ core/src/main/java/com/raywenderlich/android/majesticreader/interactors/SetOpenDocument.kt
@@ -4,5 +4,5 @@ import com.raywenderlich.android.majesticreader.data.DocumentRepository
 import com.raywenderlich.android.majesticreader.domain.Document
 
 class SetOpenDocument(private val documentRepository: DocumentRepository) {
-    suspend operator fun invoke(document: Document) = documentRepository.setOpenDocument(document)
+    operator fun invoke(document: Document) = documentRepository.setOpenDocument(document)
 }
